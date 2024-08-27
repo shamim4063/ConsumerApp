2c74183f7177427a18ca9abad60e7fa8be90f7b2
shamim4063 <shamim.hossain@efficientsoftwaresolutions.com>
2024-08-27 16:01:19 +0100

    Initial Commit

---
+++ ConsumerApp/MessageBroker/RabbitConsumerHostedService.cs
@@ -0,0 +1,58 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ConsumerApp.MessageBroker
+{
+    public class RabbitConsumerHostedService : IHostedService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private string _exchange;
+        private string _queueName;
+        public RabbitConsumerHostedService()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "95.45.224.19",
+                Port = 5672,
+                VirtualHost = "qsmartdev",
+                UserName = "essadmin",
+                Password = "123@qwe"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _queueName = "Queue1";
+            _exchange = "RnDExchange";
+
+            _channel.ExchangeDeclare(exchange: _exchange, type: ExchangeType.Fanout, durable: true);
+            _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false);
+            _channel.QueueBind(queue: _queueName, exchange: _exchange, routingKey: _queueName);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                // Process the message
+                Console.WriteLine($"Received message: {message} Time: {DateTime.Now}");
+            };
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _channel.Close();
+            _connection.Close();
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,26 @@
+using ConsumerApp.MessageBroker;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHostedService<RabbitConsumerHostedService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
